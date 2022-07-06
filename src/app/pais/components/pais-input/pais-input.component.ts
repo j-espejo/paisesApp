e9636cdb1847955c01eeb07eefd39762eae30fdc
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-07-06 10:36:20 -0400

    Componente Input y Tabla

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [],
+})
+export class PaisInputComponent {
+  termino: string = '';
+
+  buscar() {
+    console.log('hola mundo');
+    console.log(this.termino);
+  }
+}
